23c712c660611db07b36ff423d2005e834358ad9
David Morales <david_ivan2402@ciencias.unam.mx>
2020-05-13 19:50:27 -0500

    Se agregaro los cmabios

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,12 +1,12 @@
 -- Ejercicio 1.1 para miembros dados de baja
 SELECT Nombre, No_asociacion,Email, Model, ano FROM MiembroAsociacion 
 JOIN (SELECT * FROM DadoDeBaja JOIN Vehiculo ON (DadoDeBaja.Modelo == Vehiculo.modelo AND DadoDeBaja.ano == Vehiculo.ano)) ON (MiembroAsociacion.IDVehiculo = DadoDeBaja.IDVehiculoBaja)
-WHERE MiembroAsociacion.EsDueño == TRUE MiembroAsociacion.EsChofer == TRUE; 
+WHERE MiembroAsociacion.EsDueno == TRUE MiembroAsociacion.EsChofer == TRUE; 
 
 -- Ejercicio 1.2 para miembros en operacion
 SELECT Nombre, No_asociacion,Email, Model, ano FROM MiembroAsociacion 
 JOIN ( SELECT * FROM EnOperacion JOIN Vehiculo ON (EnOperacion.Modelo == Vehiculo.modelo AND EnOperacion.ano == Vehiculo.ano)) ON (MiembroAsociacion.IDVehiculo = EnOperacion.IDVehiculoBaja)
-WHERE MiembroAsociacion.EsDueño == TRUE AND MiembroAsociacion.EsChofer == TRUE; 
+WHERE MiembroAsociacion.EsDueno == TRUE AND MiembroAsociacion.EsChofer == TRUE; 
 
 -- Ejercicio 2.1 
 SELECT COUNT(*) AS total FROM  MiembroAsociacion
@@ -14,7 +14,7 @@ WHERE MiembroAsociacion.EsDueño == TRUE;
 
 -- Ejercicio 2.2 
 SELECT COUNT(*) AS total  FROM  MiembroAsociacion
-WHERE MiembroAsociacion.EsChofer == TRUE AND MiembroAsociacion.EsDueño == TRUE ;
+WHERE MiembroAsociacion.EsChofer == TRUE AND MiembroAsociacion.EsDueno == TRUE ;
 
 -- Ejercicio 2.3
 SELECT COUNT(*) AS total FROM  MiembroAsociacion
